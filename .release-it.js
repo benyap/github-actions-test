3b37234b14a0522e5c08aec4e4450d23ed59e314
Ben Yap <contact@benyap.com>
2022-06-25 12:12:17 +1000

    chore: add changelog

--- .release-it.js
+++ .release-it.js
@@ -9,6 +9,24 @@ module.exports = {
   git: {
     tag: true,
     commit: true,
-    commitMessage: "chore(release): release ${version}",
+    commitMessage: "release: ${version}",
+  },
+  plugins: {
+    "@release-it/conventional-changelog": {
+      infile: "CHANGELOG.md",
+      preset: {
+        name: "conventionalcommits",
+        types: [
+          { type: "release", section: "Releases" },
+          { type: "feat", section: "Features" },
+          { type: "fix", section: "Bug Fixes" },
+          { type: "docs", section: "Documentation" },
+          { type: "tooling", section: "Tooling" },
+          { type: "chore", section: "Internal" },
+          { type: "deps", section: "Dependencies" },
+          { type: "revert", section: "Reverts" },
+        ],
+      },
+    },
   },
 };
